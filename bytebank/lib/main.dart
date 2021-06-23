e723fbaa1da1cf08b9919aa975dd4a3ef72c82b3
Bruno Marques <brunorm1996@gmail.com>
2021-06-22 22:39:01 -0300

    Adicionados arquivos extras. Início de diagramação da tela.

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr)); // Escreva esse texto da esquerda para a direita (ltr)
